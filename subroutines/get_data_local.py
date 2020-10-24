bc8c97172a1c6bf99abc94951cffcea2e9512b72
Chelle Gentemann <35538868+cgentemann@users.noreply.github.com>
2020-10-24 08:27:01 -0700

    update to 8-day color

--- subroutines/get_data_local.py
+++ subroutines/get_data_local.py
@@ -1,5 +1,6 @@
 import xarray as xr
 import dask
+import numpy as np
 
 def get_data():
     
@@ -52,7 +53,9 @@ def get_data():
     ds_topo = ds_topo.rename({'z':'etopo_depth'})
     ds_topo.etopo_depth.attrs=tem
 
-    ds_color = xr.open_dataset('https://rsg.pml.ac.uk/thredds/dodsC/CCI_ALL-v4.2-DAILY')
+    
+    ds_color = xr.open_dataset('https://rsg.pml.ac.uk/thredds/dodsC/CCI_ALL-v5.0-8DAY')
+#    ds_color = xr.open_dataset('https://rsg.pml.ac.uk/thredds/dodsC/CCI_ALL-v4.2-DAILY')
     for var in ds_color:
         if not var=='chlor_a':
             ds_color = ds_color.drop(var)
@@ -92,3 +95,80 @@ def get_sst():
     clim_dict={'sst_clim':ds_sst_clim}
   
     return data_dict,clim_dict
+
+def get_data_360():
+    
+    #climatology years
+    cyr1,cyr2='1993-01-01','2018-12-31'
+    
+    # CCMP test
+    dir_pattern_zarr = 'F:/data/sat_data/ccmp/zarr/'
+    ds= xr.open_zarr(dir_pattern_zarr)
+    ds = ds.rename({'latitude':'lat','longitude':'lon'})
+    #ds.coords['lon'] = (ds.coords['lon'] + 180) % 360 - 180
+    #ds_ccmp = ds.sortby(ds.lon)
+    ds_ccmp = ds.drop('nobs')
+    for var in ds_ccmp:
+        tem = ds_ccmp[var].attrs
+        tem['var_name']='ccmp_'+str(var)
+        ds_ccmp[var].attrs=tem
+    ds_ccmp_clim = ds_ccmp.sel(time=slice(cyr1,cyr2))
+    ds_ccmp_clim = ds_ccmp_clim.groupby('time.dayofyear').mean('time',keep_attrs=True,skipna=False)
+    
+    # AVISO test
+    dir_pattern_zarr = 'F:/data/sat_data/aviso/zarr/'
+    ds= xr.open_zarr(dir_pattern_zarr)
+    ds = ds.rename({'latitude':'lat','longitude':'lon'})
+    #ds.coords['lon'] = (ds.coords['lon'] + 180) % 360 - 180
+    #ds = ds.sortby(ds.lon)
+    ds_aviso = ds.drop({'lat_bnds','lon_bnds','crs','err'})
+    for var in ds_aviso:
+        tem = ds_aviso[var].attrs
+        tem['var_name']='aviso_'+str(var)
+        ds_aviso[var].attrs=tem
+    ds_aviso_clim = ds_aviso.sel(time=slice(cyr1,cyr2))
+    ds_aviso_clim = ds_aviso_clim.groupby('time.dayofyear').mean('time',keep_attrs=True,skipna=False)    
+
+    #sst
+    dir_pattern_zarr = 'F:/data/sat_data/sst/cmc/zarr/'
+    ds_sst= xr.open_zarr(dir_pattern_zarr)
+    ds_sst = ds_sst.drop({'analysis_error','mask','sea_ice_fraction'})
+    ds_sst.coords['lon'] = np.mod(ds_sst['lon'], 360)
+    ds_sst = ds_sst.sortby(ds_sst.lon)
+    tem = ds_sst.analysed_sst.attrs
+    tem['var_name']='cmc_sst'
+    ds_sst.analysed_sst.attrs=tem
+    ds_sst_clim = ds_sst.sel(time=slice(cyr1,cyr2))
+    ds_sst_clim = ds_sst_clim.groupby('time.dayofyear').mean('time',keep_attrs=True,skipna=False)
+    
+    #get bathymetry from ETOPO1
+    fname_topo = 'F:/data/topo/ETOPO1_Ice_g_gmt4.grd'
+    ds = xr.open_dataset(fname_topo)
+    ds = ds.rename_dims({'x':'lon','y':'lat'}).rename({'x':'lon','y':'lat'})
+    ds.coords['lon'] = np.mod(ds['lon'], 360)
+    ds = ds.sortby(ds.lon)
+    ds_topo = ds
+    tem = ds_topo.attrs
+    ds_topo = ds_topo.rename({'z':'etopo_depth'})
+    ds_topo.etopo_depth.attrs=tem
+   
+    ds_color = xr.open_dataset('https://rsg.pml.ac.uk/thredds/dodsC/CCI_ALL-v4.2-DAILY')
+    for var in ds_color:
+        if not var=='chlor_a':
+            ds_color = ds_color.drop(var)
+    ds_color.coords['lon'] = np.mod(ds_color['lon'], 360)
+    ds_color = ds_color.sortby(ds_color.lon)
+
+    #put data into a dictionary
+    data_dict={'aviso':ds_aviso,
+               'wnd':ds_ccmp,
+               'sst':ds_sst,
+               'color':ds_color,
+              'topo':ds_topo}
+    clim_dict={'aviso_clim':ds_aviso_clim,
+               'wnd_clim':ds_ccmp_clim,
+               'sst_clim':ds_sst_clim}
+  
+    return data_dict,clim_dict
+
+#data,clim = get_data()
